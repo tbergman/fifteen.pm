8e7ac71f5f61cdc620f12ce1a38deb533b581332
Brian Abelson <brianabelson@gmail.com>
2018-07-16 11:00:49 -0400

    allow specific bitrate, downsample flys for fastest load

--- scripts/encode-release.sh
+++ scripts/encode-release.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# usage: encode-release.sh {input} {mp3-bitrate} {slug}
+
 set -x
 set -e
 
-# echo "creating public/assets/$2.ogg ... "
-# ffmpeg -i "$1"  -acodec libvorbis public/assets/$2.ogg
-echo "creating public/assets/$2.mp3 ... "
-lame "$1" "public/assets/$2.mp3" -b 320
+# echo "creating public/assets/$3.ogg ... "
+# ffmpeg -i "$1"  -acodec libvorbis public/assets/$3.ogg
+echo "creating public/assets/$3.mp3 ... "
+lame "$1" "public/assets/$3.mp3" -b $2
