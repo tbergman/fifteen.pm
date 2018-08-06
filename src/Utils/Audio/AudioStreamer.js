f5ff84d2c77308247504ea8b16c85430cbd3b279
Brian Abelson <brianabelson@gmail.com>
2018-08-05 20:31:57 -0400

    hacky fix for safari ugh

--- src/Utils/Audio/AudioStreamer.js
+++ src/Utils/Audio/AudioStreamer.js
@@ -1,3 +1,5 @@
+import {isChrome, isFirefox, isSafari} from "../BrowserDetection";
+
 class AudioStreamer {
   
   // A class which handles Web Audio API Interaction given an Audio Element
@@ -6,23 +8,25 @@ class AudioStreamer {
     this.element = audioElement
     this.context = new (window.AudioContext || window.webkitAudioContext)();
 
-    // init nodes
-    this.analyser = this.context.createAnalyser();
-    this.filter   = this.context.createBiquadFilter();
-    
-    // set defaults which will be unnoticeable
-    this.filter.frequency.value = 25000;
-    this.filter.type = "lowpass";
+    if (!isSafari) { 
+      // init nodes
+      this.analyser = this.context.createAnalyser();
+      this.filter   = this.context.createBiquadFilter();
+      this.filter.frequency.value = 25000;
+      this.filter.type = "lowpass";
+    }
     this.source = undefined;
   }
   
   // build signal path on-demand
   connect() {
-    window.onload = () => {
-      this.source = this.context.createMediaElementSource(this.element);
-      this.source.connect(this.analyser);
-      this.analyser.connect(this.filter);
-      this.filter.connect(this.context.destination);
+    if (!isSafari) { 
+      window.onload = () => {
+        this.source = this.context.createMediaElementSource(this.element);
+        this.source.connect(this.analyser);
+        this.analyser.connect(this.filter);
+        this.filter.connect(this.context.destination);
+      }
     }
   }
 
