a02900ce97d0a58f9599572cd25aca04427604c7
Brian Abelson <brianabelson@gmail.com>
2018-08-12 12:24:06 -0400

    switch between videos;
    Former-commit-id: 60156b10a20fd88aace332e408e61868dbc05a4c

--- src/Releases/Release0004.js
+++ src/Releases/Release0004.js
@@ -14,19 +14,73 @@ const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
 const VIDEO_PLAYBACK_RATE = 0.2;
 const VIDEO_NUMBER = 2;
+const ROTATION_SPEED = 0.0075;
+const A_CROSS_FADER_BUFFER = 0.35;
+const B_CROSS_FADER_BUFFER = 1.65;
+
+const SCENES = [
+  {
+    src: 'assets/straps-0.webm',
+    geometry: new THREE.SphereBufferGeometry(500, 60, 40),
+    width: 640,
+    height: 360,
+    loop: false,
+    muted: true,
+    transparent: true,
+    opacity: 0.9,
+    color: 0xFFFFFF,
+    playbackRate: 0.5,
+    target: new THREE.Vector3(4, -50, 1),
+    camera_x: 1,
+    camera_y: -48,
+    camera_z: 6,
+    scaleNotCalled: true
+  },
+  {
+    src: 'assets/straps-0.webm',
+    geometry: new THREE.BoxBufferGeometry(500, 250, 125),
+    width: 640,
+    height: 360,
+    loop: false,
+    muted: true,
+    transparent: true,
+    opacity: 0.9,
+    color: 0xFFFFFF,
+    playbackRate: 0.5,
+    target: new THREE.Vector3(4, -35, 1),
+    camera_x: 1,
+    camera_y: -48,
+    camera_z: 6,
+    scaleNotCalled: true
+  },
+  {
+    src: 'assets/straps-0.webm',
+    geometry: new THREE.SphereBufferGeometry(40, 60, 500),
+    width: 640,
+    height: 360,
+    loop: false,
+    transparent: true,
+    opacity: 0.9,
+    color: 0xFFFFFF,
+    playbackRate: 0.5,
+    target: new THREE.Vector3(4, -35, 1),
+    camera_x: 4,
+    camera_y: -50,
+    camera_z: -1,
+    scaleNotCalled: true
+  }
+];
 
 class Release0004 extends PureComponent {
   constructor() {
     super();
     this.container = document.getElementById('container');
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100);
-    this.camera.target = new THREE.Vector3(0, 0, 0);
+    this.camera.target = new THREE.Vector3(0,0,0);
     this.scene = new THREE.Scene();
-    this.video = document.createElement('video');
     this.renderer = new THREE.WebGLRenderer({antialias: true});
-    this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-    this.mesh = undefined;
+    this.renderer.setClearColor(0xffffff, 0);
     this.lat = 0;
     this.lon = 0;
     this.onMouseDownLon = 0;
@@ -43,6 +97,15 @@ class Release0004 extends PureComponent {
     this.onPointerDownLon = 0;
     this.onPointerDownLat = 0;
     this.idx = 0;
+    this.AVideo = document.createElement('video');
+    this.BVideo = document.createElement('video');
+    this.BCrossFaderOn = false;
+    this.ACrossFaderOn = false;
+    this.old_mesh = undefined;
+    // var size = 2000;
+    // var divisions = 200;
+    // var gridHelper = new THREE.GridHelper( size, divisions );
+    // this.scene.add( gridHelper );
 
   }
 
@@ -57,79 +120,137 @@ class Release0004 extends PureComponent {
     document.addEventListener('mousemove', this.onDocumentMouseMove, false);
     document.addEventListener('mouseup', this.onDocumentMouseUp, false);
     document.addEventListener('wheel', this.onDocumentMouseWheel, false);
-    this.video.addEventListener('ended', this.renderVideo, false);
+    this.AVideo.addEventListener('ended', this.removeSceneA, false);
+    this.AVideo.addEventListener('timeupdate', this.crossFaderA, false);
+    this.BVideo.addEventListener('ended', this.removeSceneB, false);
+    this.BVideo.addEventListener('timeupdate', this.crossFaderB, false);
     this.init();
     this.animate();
   }
 
   init = () => {
-    this.initAudioProps();
-    this.renderVideo();
     this.container.appendChild(this.renderer.domElement);
+    this.swapScene({channel: 'A'});
+
   }
 
-
-  renderVideo = () => {
-    let src =  `assets/straps-${this.idx}.webm`;
-    console.log('rendering video', src);    
-    if (this.idx == 1) {
-      this.geometry = new THREE.SphereBufferGeometry(500, 60, 40);
-    } else {
-      this.geometry = new THREE.BoxBufferGeometry(500, 60, 40);
+  crossFaderA = () => {
+    this.AEnd = this.AVideo.duration - A_CROSS_FADER_BUFFER;
+    // console.log('A Time', this.AVideo.currentTime );
+    // console.log('A Limit',  this.AEnd);
+    if (this.AVideo.currentTime  >= this.AEnd && this.AEnd !== NaN) {
+      // console.log(this.ACrossFaderOn);
+      if (!this.ACrossFaderOn) {
+              console.log('switch to B');
+              console.log(this.AVideo.duration);
+              this.swapScene({channel: 'B'});
+              this.ACrossFaderOn = true;
+              this.scene.remove(this.AMesh);
+      }
     }
-    // invert the geometry on the x-axis so that all of the faces point inward
-    this.geometry.scale(-1, 1, 1);
+  }
 
-    this.video.crossOrigin = 'anonymous';
-    this.video.width = 640;
-    this.video.height = 360;
-    // this.video.loop = true;
-    this.video.muted = false;
-    this.video.playbackRate = VIDEO_PLAYBACK_RATE;
+  crossFaderB = () => {
+    // console.log('B Time', this.BVideo.currentTime);
+    this.BEnd = (this.BVideo.duration - B_CROSS_FADER_BUFFER);
+    // console.log('B Limit',  this.BEnd);
+    if ( this.BVideo.currentTime  >=  this.BEnd && this.BEnd !== NaN) {
+      if (!this.BCrossFaderOn) {
+              console.log('switch to A');
+              console.log(this.BVideo.duration);
+              this.swapScene({channel: 'A'});
+              this.BCrossFaderOn = true;
+              this.scene.remove(this.BMesh);
+      }
 
-    this.video.src = src; //pano.webm';
-    this.video.setAttribute('webkit-playsinline', 'webkit-playsinline');
-    this.video.play();
-
-    this.texture = new THREE.VideoTexture(this.video);
-    this.texture.minFilter = THREE.LinearFilter;
-    this.texture.format = THREE.RGBFormat;
-    this.material = new THREE.MeshBasicMaterial({map: this.texture});
-    let newMesh = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(newMesh);
-     if (this.mesh !== undefined) {
-      this.scene.remove(this.mesh);
     }
-    this.mesh = newMesh;
-    this.idx += 1 
-    if (this.idx == VIDEO_NUMBER) { 
+  }
+  removeSceneA = () => {
+    console.log('remove A');
+    this.BCrossFaderOn = false;
+    this.scene.remove(this.AMesh);
+  }
+
+  removeSceneB = () => {
+    console.log('remove B');
+    this.ACrossFaderOn = false;
+    this.scene.remove(this.BMesh);
+    // this.swapScene();
+
+  }
+  swapScene = (opts) => {
+
+    if (this.idx >= SCENES.length) { 
       this.idx = 0;
+    }  
+    let props = SCENES[this.idx];
+    if (props.scaleNotCalled) {
+      props.geometry.scale(-1, 1, 1);
+      props.scaleNotCalled = false;
     }
-  }
+    // console.log(props);
+    // invert the geometry on the x-axis so that all of the faces point inward
 
+    // this.renderer.setPixelRatio(window.devicePixelRatio);
+    if (opts.channel == 'A') {
+      this.AVideo.src = props.src; 
+      this.AVideo.crossOrigin = 'anonymous';
+      this.AVideo.width = props.width;
+      this.AVideo.height = props.height;
+      this.AVideo.loop =  false;
+      this.AVideo.muted = props.muted;
+      this.AVideo.autoplay = true;
+      this.AVideo.playbackRate = props.playbackRate;
+      this.AVideo.currentTime = 36623;
+      this.AVideo.play();
+      this.activeChannel = opts.channel;
+      let texture = new THREE.VideoTexture(this.AVideo);
+      texture.minFilter = THREE.LinearFilter;
+      texture.format = THREE.RGBFormat;
+      let material = new THREE.MeshBasicMaterial({map: texture, transparent: props.transparent, opacity: props.opacity});
+      let mesh =  new THREE.Mesh(props.geometry, material);
+      mesh.name = 'new_vid_mesh';
+      this.camera.position.x = props.camera_x;
+      this.camera.position.y = props.camera_y;
+      this.camera.position.z = props.camera_z;
+      this.camera.target = props.target;
+      this.camera.lookAt(this.camera.target);
+      mesh.name = 'AMesh';
+      this.AMesh = mesh;
+      this.AMesh.renderOrder = 2;
+      this.scene.add(this.AMesh);
+    } else {
+      this.BVideo.src = props.src; 
+      this.BVideo.crossOrigin = 'anonymous';
+      this.BVideo.width = props.width;
+      this.BVideo.height = props.height;
+      this.BVideo.loop =  false;
+      this.BVideo.muted = props.muted;
+      this.BVideo.autoplay = true;
+      this.BVideo.playbackRate = props.playbackRate;
+      this.BVideo.currentTime = 36623;
+      this.BVideo.play();
+      this.activeChannel = opts.channel; 
+      let texture = new THREE.VideoTexture(this.BVideo);
+      texture.minFilter = THREE.LinearFilter;
+      texture.format = THREE.RGBFormat;
+      let material = new THREE.MeshBasicMaterial({map: texture, transparent: props.transparent, opacity: props.opacity});
+      let mesh =  new THREE.Mesh(props.geometry, material);
+      this.camera.position.x = props.camera_x;
+      this.camera.position.y = props.camera_y;
+      this.camera.position.z = props.camera_z;
+      this.camera.target = props.target;
+      this.camera.lookAt(this.camera.target);
+      mesh.name = 'vid_mesh';
+      this.BMesh = mesh;
+      this.BMesh.renderOrder = 1;
+      this.AMesh.renderOrder = 0;
+      this.scene.add(this.BMesh);
+      
+    }
 
-
-  initAudioProps = () => {
-    this.audioStream = new AudioStreamer(this.audioElement);
-    this.audioStream.analyser.fftSize = 256;
-    this.volArray = new Uint8Array(this.audioStream.analyser.fftSize);
-    this.numVolBuckets = 4;
-    this.volBucketSize = this.volArray.length / this.numVolBuckets;
-
-    this.freqArray = new Uint8Array(this.audioStream.analyser.frequencyBinCount);
-    this.numFreqBuckets = 64;
-    this.freqBucketSize = this.freqArray.length / this.numFreqBuckets;
-
-    // this.bassIndex = 0; // the vol bucket indices, assigned by freq range
-    // this.midIndex1 = 1;
-    // this.midIndex2 = 2;
-    // this.trebIndex = 3;
-    // this.bassThresh = 100;
-    // this.midThresh = 130;
-    // this.trebThresh = 140.0;
-    // this.normalizingConst = 200.0;
-
-
+  this.idx += 1;
+    
   }
 
   componentWillUnmount() {
@@ -139,8 +260,10 @@ class Release0004 extends PureComponent {
     window.removeEventListener("touchstart", this.onTouch, false);
     window.removeEventListener("touchend", this.onTouchEnd, false);
     window.removeEventListener("load", this.onLoad, false);
-    this.video.removeEventListener('ended', this.renderVideo, false);
-
+    this.AVideo.removeEventListener('ended', this.removeSceneA, false);
+    this.AVideo.removeEventListener('timeupdate', this.crossFaderA, false);
+    this.BVideo.removeEventListener('ended', this.removeSceneB, false);
+    this.BVideo.removeEventListener('timeupdate', this.crossFaderB, false);
     this.container.removeChild(this.renderer.domElement);
   }
 
@@ -157,6 +280,7 @@ class Release0004 extends PureComponent {
     if (this.isUserInteracting === true) {
       this.lon = (this.onPointerDownPointerX - event.clientX) * 0.1 + this.onPointerDownLon;
       this.lat = (event.clientY - this.onPointerDownPointerY) * 0.1 + this.onPointerDownLat;
+
     }
   }
 
@@ -177,41 +301,10 @@ class Release0004 extends PureComponent {
     this.camera.updateProjectionMatrix();
   }, 50);
 
-  setMouseCoords = (x, y) => {
-    this.mouse.x = (x / window.innerWidth) * 2 - 1;
-    this.mouse.y = -(y / window.innerHeight) * 2 + 1;
-    this.mouseMoved = true;
-  }
 
   onLoad = (event) => {
-    this.audioStream.connect()
+    // this.audioStream.connect();
   }
-  //
-  // onMouseMove = (event) => {
-  //   this.setMouseCoords(event.clientX, event.clientY);
-  // };
-  //
-  // onTouch = (event) => {
-  //   // event.preventDefault();
-  //   if (event.touches) {
-  //     this.setMouseCoords(event.touches[0].clientX, event.touches[0].clientY);
-  //   }
-  // };
-  //
-  // // turn off filter if no touch
-  // onTouchEnd = (event) => {
-  //   // event.preventDefault();
-  //   this.mouse.x = 10000;
-  //   this.mouse.y = 10000;
-  // }
-  //
-  // onWindowResize = debounce(() => {
-  //   const WIDTH = window.innerWidth;
-  //   const HEIGHT = window.innerHeight;
-  //   this.renderer.setSize(WIDTH, HEIGHT);
-  //   this.camera.aspect = WIDTH / HEIGHT;
-  //   this.camera.updateProjectionMatrix();
-  // }, 50);
 
   stop = () => {
     cancelAnimationFrame(this.frameId);
@@ -222,52 +315,27 @@ class Release0004 extends PureComponent {
     this.renderScene();
   }
 
-  // getAverage = (start, end, array) => {
-  //   let values = 0;
-  //   let average;
-  //   // get all the frequency amplitudes
-  //   for (let i = start; i <= end; i++) {
-  //     values += array[i];
-  //   }
-  //   average = values / (end - start);
-  //   return average;
-  // }
-  //
-  // getBuckets = (numBuckets, bucketSize, array) => {
-  //   let buckets = [];
-  //   for (let i = 0; i < numBuckets; i++) {
-  //     let start = i * bucketSize;
-  //     let end = (i + 1) * bucketSize - 1;
-  //     buckets.push(this.getAverage(start, end, array))
-  //   }
-  //   return buckets;
-  // }
-  //
-  // getFreqBuckets = () => {
-  //   this.audioStream.analyser.getByteFrequencyData(this.freqArray);
-  //   return this.getBuckets(this.numFreqBuckets, this.freqBucketSize, this.volArray)
-  // }
-  //
-  // getVolBuckets = () => {
-  //   this.audioStream.analyser.getByteTimeDomainData(this.volArray);
-  //   return this.getBuckets(this.numVolBuckets, this.volBucketSize, this.volArray)
-  // }
-
-  renderByTrackSection = (currentTime) => {
-  }
-
   renderScene = () => {
-    this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
-    this.phi = THREE.Math.degToRad( 90 - this.lat );
-    this.theta = THREE.Math.degToRad( this.lon );
 
-    this.camera.position.x = this.distance * Math.sin( this.phi ) * Math.cos( this.theta );
-    this.camera.position.y = this.distance * Math.cos( this.phi );
-    this.camera.position.z = this.distance * Math.sin( this.phi ) * Math.sin( this.theta );
-
-    this.camera.lookAt( this.camera.target );
-
+    if (this.isUserInteracting) {
+      this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
+      this.phi = THREE.Math.degToRad( 90 - this.lat );
+      this.theta = THREE.Math.degToRad( this.lon );
+      this.camera.position.x = this.distance * Math.sin( this.phi ) * Math.cos( this.theta );
+      this.camera.position.y = this.distance * Math.cos( this.phi );
+      this.camera.position.z = this.distance * Math.sin( this.phi ) * Math.sin( this.theta );
+    } else {
+      let x = this.camera.position.x;
+      let z = this.camera.position.z;
+      this.camera.position.x = x * Math.cos(ROTATION_SPEED) + z * Math.sin(ROTATION_SPEED);
+      this.camera.position.z = z * Math.cos(ROTATION_SPEED) - x * Math.sin(ROTATION_SPEED);
+    }
+    // console.log(this.camera.position.x, this.camera.position.y, this.camera.position.z);
+    this.camera.lookAt(this.camera.target);
     this.renderer.render(this.scene, this.camera);
+
+    // console.log(this.camera.position.x, this.camera.position.y, this.camera.position.z);
+    // console.log(this.distance, this.lat, this.lon);
   }
 
   render() {
@@ -288,5 +356,6 @@ class Release0004 extends PureComponent {
     );
   }
 }
+
 
 export default Release0004;
