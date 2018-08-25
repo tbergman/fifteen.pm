30df84c60dbda2ed73393a7135940ec934c4b69c
Brian Abelson <brianabelson@gmail.com>
2018-08-25 17:55:48 -0400

    switch to subdir

--- scripts/upload-assets.sh
+++ scripts/upload-assets.sh
@@ -3,5 +3,5 @@
 set -x
 set -e
 
-aws --profile $1 s3 sync public/assets/ s3://assets.globally.ltd/
+aws --profile $1 s3 sync public/assets/releases/ s3://assets.globally.ltd/
 aws --profile $1 cloudfront create-invalidation --distribution-id E2CO6ZKFCFUALS --paths '/*' > /dev/null
