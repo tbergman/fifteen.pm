9f8a2fa1146aadf69831ae0a10098c7f5a3d9124
Brian Abelson <brianabelson@gmail.com>
2018-08-26 17:05:29 -0400

    vid world
    Former-commit-id: 60660332029e1bb44d00942e3450481750c1945d

--- src/Releases/Release0004Video.js
+++ src/Releases/Release0004Video.js
@@ -9,6 +9,7 @@ import {isMobile} from "../Utils/BrowserDetection";
 import debounce from "lodash/debounce";
 
 const BPM = 130;
+const RANGE = 1000;
 const BEAT_TIME = (60 / BPM);
 const SCREEN_WIDTH = window.innerWidth;
 const SCREEN_HEIGHT = window.innerHeight;
@@ -16,100 +17,171 @@ const SCREEN_HEIGHT = window.innerHeight;
 const BODEGAS = [
   {
     src: 'assets/releases/4/videos/er-99-cts-broadway-1.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [500, 0, 0],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
     src: 'assets/releases/4/videos/er-bag-1.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [-500, 0, 0],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
     src: 'assets/releases/4/videos/er-bodega-chill-2.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [0, 500, 0],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
     src: 'assets/releases/4/videos/er-big-boi-bitcoin-brian.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [0, -500, 0],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
     src: 'assets/releases/4/videos/er-cholulita-bite.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [0, 0, 500],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
     src: 'assets/releases/4/videos/er-broadway-tvs-n-elbows.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [0, 0, -500],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
     src: 'assets/releases/4/videos/er-broadway-spread.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [250, 0, 0],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(1, 0, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
     src: 'assets/releases/4/videos/er-broadway-bongs.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [-250, 0, 0],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(1, 0, 0).normalize(),
+    angle: 0.01,
     color: 0xFFFFFF,
     playbackRate: 1
   },
-  {
-    src: 'assets/releases/4/videos/er-broadway-fridge-door.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
-    position: [0, 250, 0],
-    transparent: false,
-    opacity: 1,
-    color: 0xFFFFFF,
-    playbackRate: 1
-  },
+  // {
+  //   src: 'assets/releases/4/videos/er-broadway-fridge-door.webm',
+  //   geometry: new THREE.SphereBufferGeometry(125, 125, 125),
+  //   position: [0, 250, 0],
+  //   transparent: false,
+  //   opacity: 1,
+  //    visible: false,
+  //   color: 0xFFFFFF,
+  //   playbackRate: 1
+  // },
   {
     src: 'assets/releases/4/videos/er-day-and-night-pringles.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [0, -250, 0],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
+    visible: false,
     color: 0xFFFFFF,
     playbackRate: 1
   },
   {
-    src: 'assets/releases/4/videos/er-eric-mini-makret-central-ave.webm',
-    geometry: new THREE.SphereBufferGeometry(100, 100, 100),
+    src: 'assets/releases/4/videos/er-eric-mini-market-central-ave.webm',
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
     position: [0, 0, 250],
     transparent: false,
     opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
+    visible: false,
+    color: 0xFFFFFF,
+    playbackRate: 1
+  },
+  {
+    src: 'assets/releases/4/videos/er-broadway-bougie-ceiling-fan.webm',
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
+    position: [250, 250, 0],
+    transparent: false,
+    opacity: 1,
+    axis: new THREE.Vector3(0, 1, 0).normalize(),
+    angle: 0.01,
+    visible: false,
+    color: 0xFFFFFF,
+    playbackRate: 1
+  },
+  {
+    src: 'assets/releases/4/videos/er-mr-kiwi-cat-in-the-cabbage.webm',
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
+    position: [-250, 250, 0],
+    transparent: false,
+    opacity: 1,
+    axis: new THREE.Vector3(0, 0, 1).normalize(),
+    angle: 0.01,
+    visible: false,
+    color: 0xFFFFFF,
+    playbackRate: 1
+  },
+  {
+    src: 'assets/releases/4/videos/er-pomegranite-deli.webm',
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
+    position: [-250, -250, 0],
+    transparent: false,
+    opacity: 1,
+    axis: new THREE.Vector3(0, 0, 1).normalize(),
+    angle: 0.01,
+    visible: false,
+    color: 0xFFFFFF,
+    playbackRate: 1
+  },
+  {
+    src: 'assets/releases/4/videos/er-99-cts-broadway-5.webm',
+    geometry: new THREE.SphereBufferGeometry(125, 125, 125),
+    position: [-250, -250, 0],
+    transparent: false,
+    opacity: 1,
+    axis: new THREE.Vector3(0, 0, 1).normalize(),
+    angle: 0.01,
+    visible: false,
     color: 0xFFFFFF,
     playbackRate: 1
   }
@@ -120,7 +192,7 @@ class Release0004Video extends PureComponent {
     super();
     this.startTime = new Date();
     this.scene = new THREE.Scene();
-    this.scene.background = new THREE.Color(0xFFFFFF);
+    this.scene.background = new THREE.Color(0x000000);
     this.camera = new THREE.PerspectiveCamera(80, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 3000);
     this.camera.position.x = 200;
     this.camera.position.y = 600;
@@ -128,24 +200,35 @@ class Release0004Video extends PureComponent {
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+    this.quaternion = new THREE.Quaternion();
     this.controls = new OrbitControls(this.camera);
-    this.controls.target.set(0, 0, 0);
-    this.controls.update();
-    var size = 2000;
-    var divisions = 200;
-    var gridHelper = new THREE.GridHelper( size, divisions );
-    this.scene.add( gridHelper );
-    this.videos = [];
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector3();
+    // var size = 2000;
+    // var divisions = 200;
+    // var gridHelper = new THREE.GridHelper( size, divisions );
+    // this.scene.add( gridHelper );
+    this.videos = new THREE.Object3D();
   }
 
   componentDidMount() {
     window.addEventListener("resize", this.onWindowResize, false);
-    window.addEventListener("mousemove", this.onMouseMove, false);
-    window.addEventListener("touchstart", this.onTouch, false);
-    window.addEventListener("touchend", this.onTouchEnd, false);
+    window.addEventListener('mousemove', this.onMouseMove, false );
+    window.addEventListener('mousedown', this.onMouseDown, false );
     window.addEventListener("load", this.onLoad, false);
     this.init();
     this.animate();
+  }
+
+  componentWillUnmount() {
+    this.stop();
+    window.removeEventListener("resize", this.onWindowResize, false);
+    window.removeEventListener("mousemove", this.onMouseMove, false);
+    window.removeEventListener('mousedown', this.onMouseDown, false );
+    window.removeEventListener("touchstart", this.onTouch, false);
+    window.removeEventListener("touchend", this.onTouchEnd, false);
+    window.removeEventListener("load", this.onLoad, false);
+    this.container.removeChild(this.renderer.domElement);
   }
 
   init = () => {
@@ -157,13 +240,12 @@ class Release0004Video extends PureComponent {
   initVideos = () => {
     for (let i = 0; i < BODEGAS.length; i++) {
       let props = BODEGAS[i];
-      let videoMesh = this.initVideoMesh(props);
-      this.videos.push(videoMesh);
-      this.scene.add(videoMesh);
-      videoMesh.position.set(...props.position);
       props.geometry.scale(-1, 1, 1);
-
+      let videoMesh = this.initVideoMesh(props);
+      videoMesh.position.set(...props.position);
+      this.videos.add(videoMesh);
     }
+    this.scene.add(this.videos);
   };
 
   initVideoMesh = (props) => {
@@ -172,11 +254,8 @@ class Release0004Video extends PureComponent {
     video.crossOrigin = 'anonymous';
     video.loop = true;
     video.muted = true;
-    video.autoplay = true;
+    // video.autoplay = true;
     video.playbackRate = props.playbackRate;
-    video.play();
-    console.log('INIT:')
-    console.log(video.src);
     let texture = new THREE.VideoTexture(video);
     texture.minFilter = THREE.LinearFilter;
     texture.format = THREE.RGBFormat;
@@ -185,11 +264,23 @@ class Release0004Video extends PureComponent {
       transparent: props.transparent,
       opacity: props.opacity
     });
-    let mesh = new THREE.Mesh(props.geometry, material);
-    mesh.renderOrder = 1;
-    return mesh;
+    let videoMesh = new THREE.Mesh(props.geometry, material);
+    videoMesh.renderOrder = 1;
+    videoMesh.rotation.y = Math.sin(0.6);
+    videoMesh.userData.video = video;
+    videoMesh.userData.props = props;
+    return videoMesh;
   }
 
+  rotateVideos = () => {
+
+    for (let i =0; i < this.videos.children.length; i++) {
+      this.quaternion.setFromAxisAngle(
+        this.videos.children[i].userData.props.axis,
+        this.videos.children[i].userData.props.angle);
+      this.videos.children[i].applyQuaternion(this.quaternion);
+    }
+  }
 
   initAudioProps = () => {
     this.audioStream = new AudioStreamer(this.audioElement);
@@ -202,16 +293,6 @@ class Release0004Video extends PureComponent {
     this.freqBucketSize = this.freqArray.length / this.numFreqBuckets;
   }
 
-  componentWillUnmount() {
-    this.stop();
-    window.removeEventListener("resize", this.onWindowResize, false);
-    window.removeEventListener("mousemove", this.onMouseMove, false);
-    window.removeEventListener("touchstart", this.onTouch, false);
-    window.removeEventListener("touchend", this.onTouchEnd, false);
-    window.removeEventListener("load", this.onLoad, false);
-    this.container.removeChild(this.renderer.domElement);
-  }
-
   onWindowResize = debounce(() => {
     const WIDTH = window.innerWidth;
     const HEIGHT = window.innerHeight;
@@ -220,10 +301,29 @@ class Release0004Video extends PureComponent {
     this.camera.updateProjectionMatrix();
   }, 50);
 
-  setMouseCoords = (x, y) => {
-    this.mouse.x = (x / window.innerWidth) * 2 - 1;
-    this.mouse.y = -(y / window.innerHeight) * 2 + 1;
-    this.mouseMoved = true;
+  onMouseMove = ( e ) => {
+    this.mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    this.mouse.y = -(e.clientY / window.innerHeight) * 2 + 1;
+  }
+
+  onMouseDown  = ( e ) => {
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    let intersects = this.raycaster.intersectObjects( this.videos.children );
+    // stop all other videos
+    for (var i = 0; i < this.videos.children.length; i++) {
+      this.videos.children[i].userData.video.pause();
+    }
+    // start the video we've clicked on an zoom to camera.
+    for( var i = 0; i < intersects.length; i++ ) {
+      var intersection = intersects[ i ];
+      this.camera.position.x = intersection.object.position.x;
+      this.camera.position.y = intersection.object.position.y;
+      this.camera.position.z = intersection.object.position.z;
+      intersection.object.userData.video.play();
+      // this.controls.target.x = intersection.object.position.x;
+      // this.controls.target.y = intersection.object.position.y;
+      // this.controls.target.z = intersection.object.position.z;
+    }
   }
 
   onLoad = (event) => {
@@ -242,15 +342,13 @@ class Release0004Video extends PureComponent {
   animate = () => {
     this.frameId = window.requestAnimationFrame(this.animate);
     this.renderScene();
-    this.updateOrbitControls();
-
-  }
-
-  renderByTrackSection = (currentTime) => {
   }
 
   renderScene = () => {
+    this.rotateVideos();
+    this.updateOrbitControls();
     this.renderer.render(this.scene, this.camera);
+
   }
 
   render() {
