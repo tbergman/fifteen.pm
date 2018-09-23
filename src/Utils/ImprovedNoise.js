c24c2dbd885699d6023f2b71acae92180d573eaa
Jeremy Krinsley <krinsley@gmail.com>
2018-09-23 17:19:59 -0400

    Cleaned up most warnings

--- src/Utils/ImprovedNoise.js
+++ src/Utils/ImprovedNoise.js
@@ -34,8 +34,8 @@ export const ImprovedNoise = () => {
   function grad(hash, x, y, z) {
 
     var h = hash & 15;
-    var u = h < 8 ? x : y, v = h < 4 ? y : h == 12 || h == 14 ? x : z;
-    return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
+    var u = h < 8 ? x : y, v = h < 4 ? y : h === 12 || h === 14 ? x : z;
+    return ((h&1) === 0 ? u : -u) + ((h&2) === 0 ? v : -v);
 
   }
 
