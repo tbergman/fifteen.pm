8fb6ef24250920b5dd31f8ae83d15581588f9b45
Karen Aragon <karagon@schoology.com>
2018-10-03 22:11:55 -0400

    feat(0005): mirror test

---
+++ src/Releases/Release0005/constants.js
@@ -0,0 +1,11 @@
+
+export const CONSTANTS = {
+    // Get window dimension
+    ww: document.documentElement.clientWidth || document.body.clientWidth,
+    wh: window.innerHeight,
+    textures: {
+        galaxy: {
+            url: "assets/circuit_pattern.png"
+        },
+    },
+};
