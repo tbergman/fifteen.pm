550d8d2a85936962b751ef64fead5896f5b9b62c
Jeremy Krinsley <krinsley@gmail.com>
2018-10-09 22:46:32 -0400

    Add playlist component

---
+++ src/PlaylistConstants.js
@@ -0,0 +1,17 @@
+
+
+export const PLAYLISTS = {
+  4: {
+    artist: "JON CANNON",
+    tracks: [
+      {
+        title: "Nothing (Blood)",
+        id: "507660189"
+      },
+      {
+        title: "Other track",
+        id: "482138307"
+      }
+    ]
+  }
+}
