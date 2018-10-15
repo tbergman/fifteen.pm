ba1fb16ac6be24d45c72ebada8a6b6913420fd07
houseofaragon <saintkaren@gmail.com>
2018-10-14 23:26:30 -0400

    feat(0005): update skybox and tunnel mesh
    Former-commit-id: a46cde8b1a873aa1c288be4bc91feeff3d4f3dea

--- src/Releases/Release0005/Utils/cameraViews.js
+++ src/Releases/Release0005/Utils/cameraViews.js
@@ -1,15 +1,15 @@
 import * as THREE from "three";
 
 function getView1(camera) {
-    camera.position.set(1.5, 1.3, 5.1);
-
-    camera.rotation.x = 0;
-    camera.rotation.y = 1.3;
-    camera.rotation.z = 0;
+    camera.position.x = 0;
+    // camera.rotation.y = Math.PI / 2;
+    camera.position.z = -10;
+    camera.position.y = 10;
+    camera.lookAt(new THREE.Vector3());
     camera.fov = 60;
-
-    this.isView1 = true;
+    this.isView1 = false;
     return camera;
+
 }
 
 function getView2(camera) {
@@ -37,9 +37,9 @@ function getView3(camera) {
 }
 
 function getView4(camera) {
-    camera.position.set(-12.5, 2.9, 14.7);
+    camera.position.set(-12.5, 2.9, -14.7);
 
-    camera.rotation.x = -0.15;
+    camera.rotation.x = 1;
     camera.rotation.y = -0.9;
     camera.rotation.z = -0.14;
     camera.fov = 60;
@@ -63,13 +63,25 @@ function getView5(camera) {
 function getView6(camera) {
     camera.position.x = 0;
     // camera.rotation.y = Math.PI / 2;
-    camera.position.z = 10;
-    camera.position.y = 20;
+    camera.position.z = -1;
+    camera.position.y = 10;
     camera.lookAt(new THREE.Vector3());
-    camera.fov = 8;
+    camera.fov = 16;
     this.isView1 = false;
     return camera;
 }
+
+// function getView7(camera) {
+//     camera.position.set(-1.5, 0.3, 2.1);
+//
+//     camera.rotation.x = 0;
+//     camera.rotation.y = 0.04;
+//     camera.rotation.z = 0;
+//     camera.fov = 60;
+//
+//     this.isView1 = true;
+//     return camera;
+// }
 
 function getView7(camera) {
     camera.position.set(-1.5, 1.3, 5.1);
@@ -83,11 +95,21 @@ function getView7(camera) {
     return camera;
 }
 
+// really nice closeup
+// camera.position.x = 0;
+// // camera.rotation.y = Math.PI / 2;
+// camera.position.z = -10;
+// camera.position.y = 10;
+// camera.lookAt(new THREE.Vector3());
+// camera.fov = 60;
+// this.isView1 = false;
+// return camera;
+
 export const cameraViews = [
     getView1,
     getView2,
     getView3,
-    getView4,
+    //getView4,
     getView5,
     getView6,
     getView7,
