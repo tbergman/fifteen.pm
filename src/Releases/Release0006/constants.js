f5318647039a0288068d60cf34b7c8e4091af43d
Jeremy Krinsley <krinsley@gmail.com>
2018-11-14 22:52:30 -0500

    Sequencing

--- src/Releases/Release0006/constants.js
+++ src/Releases/Release0006/constants.js
@@ -1,8 +1,24 @@
 import * as THREE from "three";
 
 export const CONSTANTS = {
+  cameraStartPos: new THREE.Vector3(-12.5, -8, -30),
+  frameRate: 25,
   purbaShootTimes: [
-    2
+    2,
+    20,
+    23,
+    29,
+    32,
+    35,
+    44,
+    47,
+    108,
+    71,
+    105,
+    110,
+    114,
+    116,
+    135
   ],
   phurbaCombos: [
     {
