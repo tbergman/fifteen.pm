4302162105dede50dcf4ce0f45e47f3a194c3dcd
Jeremy Krinsley <krinsley@gmail.com>
2018-11-15 01:58:48 -0500

    More strobe and phurba events
    Former-commit-id: efb4fc61729a12637139d5771d1a4e4dc9eccf58

--- src/Releases/Release0006/constants.js
+++ src/Releases/Release0006/constants.js
@@ -3,43 +3,28 @@ import * as THREE from "three";
 export const CONSTANTS = {
   cameraStartPos: new THREE.Vector3(-12.5, -8, -30),
   frameRate: 25,
+  numPhurbas: 1,
   purbaShootTimes: [
-    2,
-    20,
+    18,
     23,
     29,
     32,
-    35,
+    39,
     44,
     47,
-    108,
     71,
-    105,
-    110,
-    114,
-    116,
-    135
+    108,
+    166,
+    186,
+    219,
+    226
   ],
-  phurbaCombos: [
-    {
-      start: new THREE.Vector3(-1000, -1000, -1000),
-      end: new THREE.Vector3(1000, 1000, 1000),
-    },
-    {
-      start: new THREE.Vector3(-1000, -1000, 1000),
-      end: new THREE.Vector3(1000, 1000, -1000),
-    },
-    {
-      start: new THREE.Vector3(-1000, 1000, 1000),
-      end: new THREE.Vector3(1000, -1000, -1000),
-    },
-    {
-      start: new THREE.Vector3(1000, 1000, 1000),
-      end: new THREE.Vector3(-1000, -1000, -1000),
-    },
-    {
-      start: new THREE.Vector3(1000, -1000, 1000),
-      end: new THREE.Vector3(-1000, 1000, -1000),
-    }
+  strobeOn: [
+    47,
+    60,
+    195,
+    207,
+    235,
+    247
   ]
 }
