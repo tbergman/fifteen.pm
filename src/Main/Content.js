1a5a128876cb7b5bfe260f7ec2aaa102615e25d7
Brian Abelson <brianabelson@gmail.com>
2019-02-21 21:30:20 -0800

    switch back to jons songs

--- src/Main/Content.js
+++ src/Main/Content.js
@@ -106,34 +106,33 @@ export const CONTENT = {
   "/4": {
     artist: "JON CANNON",
     tracks: [
-      { // These tracks are useful for devving on the player
-        title: "Fake short song 1",
-        id: "58432359" //287949388"
+      // These tracks are useful for devving on the player
+      //   title: "Fake short song 1",
+      //   id: "58432359" //287949388"
+      // },
+      // {
+      //   title: "Fake short song 2",
+      //   id: "177365673"
+      // },
+      // {
+      //   title: "Fake short song 3",
+      //   id: "177365185"
+      // },
+      // {
+      //   title: "Fake short song 4",
+      //   id: "177364838"
+      {
+        title: "Nothing (Blood)",
+        id: "507660189"
       },
       {
-        title: "Fake short song 2",
-        id: "177365673"
+        title: "Miracle Center",
+        id: "513518607"
       },
       {
-        title: "Fake short song 3",
-        id: "177365185"
-      },
-      {
-        title: "Fake short song 4",
-        id: "177364838"
+        title: "Finesse",
+        id: "513518595"
       }
-      // {
-      //   title: "Nothing (Blood)",
-      //   id: "507660189"
-      // },
-      // {
-      //   title: "Miracle Center",
-      //   id: "513518607"
-      // },
-      // {
-      //   title: "Finesse",
-      //   id: "513518595"
-      // }
     ],
     theme: {
       message:
