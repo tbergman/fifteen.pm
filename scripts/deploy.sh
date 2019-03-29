42ddde004244d38741439badd0698a255ea60f1f
Brian Abelson <brianabelson@gmail.com>
2019-03-28 22:18:30 -0700

    ci suite

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,13 @@ set -x
 set -e
 
 # # build the app
-node scripts/build.js
+yarn build
 # # gzip all assets that aren't movies
 cd build/ && find . -type f ! -path '*.mp4*' ! -path '*.webm*' -exec gzip -9 "{}" \; -exec mv "{}.gz" "{}" \;
 cd ../
 # push gzipped assets 
-aws --profile $1 s3 sync build/ $2 --exclude '*.mp4*' --exclude '*.webm*' --acl public-read --content-encoding gzip
+aws s3 sync build/ $1 --exclude '*.mp4*' --exclude '*.webm*' --acl public-read --content-encoding gzip
 # push non-gzipped assets
-aws --profile $1 s3 sync build/ $2 --include '*.mp4*' --include '*.webm*' --acl public-read
+aws s3 sync build/ $1 --include '*.mp4*' --include '*.webm*' --acl public-read
 # refresh cloudfront
-aws --profile $1 cloudfront create-invalidation --distribution-id $3 --paths '/*' > /dev/null
+aws cloudfront create-invalidation --distribution-id $2 --paths '/*' > /dev/null
