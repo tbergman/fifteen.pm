46766f001ca3e0116d7ba97ab68a5f79d1140268
jaak <dev@globally.ltd>
2019-05-30 20:29:06 -0400

    Add greem

--- src/Home/HomeMobile.js
+++ src/Home/HomeMobile.js
@@ -245,6 +245,7 @@ class HomeMobile extends Component {
           <li><a href="/5">Plebeian</a></li>
           <li><a href="/6">vveiss</a></li>
           <li><a href="/7">Jon Fay</a></li>
+          <li><a href="/8">Greem Jellyfish</a></li>
         </ul>
     </div>);
   }
