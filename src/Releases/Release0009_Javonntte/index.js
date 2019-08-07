2f8c117483eaa9ef3493cc7cb101b4a6d58d5212
jaak <dev@globally.ltd>
2019-08-07 15:00:45 -0400

    React three fiber example

--- src/Releases/Release0009_Javonntte/index.js
+++ src/Releases/Release0009_Javonntte/index.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import * as THREE from 'three';
+import { Canvas } from 'react-three-fiber';
 
-export default function Release0009_Javonntte({}) {
-    return (<div>Javonnttee</div>)
+function Thing({ vertices }) {
+
+    return (
+        <group ref={ref => console.log('we have access to the instance')}>
+            <line>
+                <geometry
+                    attach="geometry"
+                    vertices={vertices.map(v => new THREE.Vector3(...v))}
+                    onUpdate={self => (self.verticesNeedUpdate = true)}
+                />
+                <lineBasicMaterial attach="material" color="black" />
+            </line>
+            <mesh
+                onClick={e => console.log('click')}
+                onPointerOver={e => console.log('hover')}
+                onPointerOut={e => console.log('unhover')}>
+                <circleGeometry attach="geometry" />
+                <meshBasicMaterial attach="material" color="white" opacity={0.5} transparent />
+            </mesh>
+        </group>
+    )
+}
+
+export default function Release0009_Javonntte({ }) {
+    return (
+        <Canvas>
+            <Thing vertices={[[-1, 0, 0], [0, 1, 0], [1, 0, 0], [0, -1, 0], [-1, 0, 0]]} />
+        </Canvas>
+    );
 }
