e0662c5e5462274883c5d9f939a2c3a1b70c2226
jaak <dev@globally.ltd>
2019-08-08 18:38:18 -0400

    Working tiles (messy version)

--- src/Releases/Release0009_Javonntte/index.js
+++ src/Releases/Release0009_Javonntte/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as THREE from 'three';
 import { extend, useThree, useRender, Canvas } from 'react-three-fiber';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
@@ -6,13 +6,13 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 extend({ OrbitControls });
 
 function Controls() {
-    const ref = useRef();
-    const { camera } = useThree();
-    useRender(() => ref.current && ref.current.update());
+    const controls = useRef();
+    const { camera, canvas } = useThree();
+    useRender(() => { controls.current && controls.current.update() });
     return (
         <orbitControls
-            ref={ref}
-            args={[camera]}
+            ref={controls}
+            args={[camera, canvas]}
             enableDamping
             dampingFactor={0.1}
             rotateSpeed={0.1}
@@ -20,44 +20,133 @@ function Controls() {
     );
 }
 
-
-//https://codesandbox.io/s/vigorous-poitras-0e02y
-function Thing({ vertices }) {
-
+function Tile({ pos, time, name }) {
     return (
-        <group ref={ref => console.log('we have access to the instance')}>
-            <line>
-                <geometry
-                    attach="geometry"
-                    vertices={vertices.map(v => new THREE.Vector3(...v))}
-                    onUpdate={self => (self.verticesNeedUpdate = true)}
-                />
-                <lineBasicMaterial attach="material" color="black" />
-            </line>
+        <group userData={{ time: time, name: name }}>
             <mesh
-                onClick={e => console.log('click')}
-                onPointerOver={e => console.log('hover')}
-                onPointerOut={e => console.log('unhover')}>
-                <circleGeometry attach="geometry" />
-                <meshBasicMaterial attach="material" color="white" opacity={0.5} transparent />
+                rotation={new THREE.Euler(-90, 0, 0)}
+                position={pos}
+            >
+                <planeGeometry attach="geometry" />
+                <meshBasicMaterial attach="material" color="white" />
             </mesh>
         </group>
     )
 }
 
-const glConfig = {
-    gammaInput: true,
-    gammaOutput: true
-    //physicallyCorrectLights: true
-};
+
+function TileGenerator({ halfTilesX, halfTilesZ, tileSize }) {
+    // let tiles = {};
+    const [tiles, setTiles] = useState({});
+    const [updateTime, setUpdateTime] = useState(0);
+    const [startPos, setStartPos] = useState(new THREE.Vector3);
+    const { camera, scene } = useThree();
+
+    useEffect(() => {
+        addTiles();
+        // refreshTileMap();
+        setStartPos(camera.position);
+    }, [updateTime]);
+
+    function nameTile(pos) {
+        return "Tile_" + pos.x + "_" + pos.z;
+    }
+
+    function destroyTile(tile) {
+        scene.remove(tile);
+        delete tiles[tile.name];
+    }
+
+    function addTiles() {
+        // force integer position rounded to nearest tilesize (integers for navigation)
+        const cameraX = Math.floor(camera.position.x / tileSize) * tileSize;
+        const cameraZ = Math.floor(camera.position.z / tileSize) * tileSize;
+        for (let x = -halfTilesX; x < halfTilesX; x++) {
+            for (let z = -halfTilesZ; z < halfTilesZ; z++) {
+                const pos = new THREE.Vector3((x * tileSize + cameraX), 0, (z * tileSize + cameraZ));
+                addTile(pos);
+            }
+        }
+        setTiles(tiles);
+    }
+
+    function addTile(pos) {
+        const tilename = nameTile(pos);
+        if (!tiles.hasOwnProperty(tilename)) {
+            // const tile = Tile({ pos: pos, time: updateTime, name: tilename })
+            tiles[tilename] = { pos: pos, time: updateTime, name: tilename };//tile;
+        } else {
+            tiles[tilename].time = updateTime;
+
+        }
+    }
+
+    function refreshTileMap() {
+        const newTiles = {}
+        const tileObjs = Object.values(tiles);
+        for (const tile of tileObjs) {
+            if (tile.time != updateTime) {
+                destroyTile(tile);
+            } else {
+                newTiles[tile.name] = tile;
+            }
+        }
+        setTiles(newTiles);
+    }
+
+    useRender((state, time) => {
+        // how far has player moved in x and z
+        const xMove = camera.position.x - startPos.x;
+        const zMove = camera.position.z - startPos.z;
+        if (Math.abs(xMove) >= tileSize || Math.abs(zMove) >= tileSize) {
+            setUpdateTime(time);
+            // addTiles();
+            // // refreshTileMap();
+            // setStartPos(camera.position);
+        }
+    })
+    // TODO why does this get logged three times on mount?
+    // const tileObjs = Object.values(tiles);
+    // const generatedTiles = <>{tileObjs.map((tile) => tile)}</>;
+    const generatedTiles = Object.values(tiles).map(function (tile) {
+        return (
+            <group
+                key={tile.name}
+                userData={{ time: tile.time, name: tile.name }}>
+                <mesh
+                    rotation={new THREE.Euler(-90, 0, 0)}
+                    position={tile.pos}
+                >
+                    <planeGeometry attach="geometry" />
+                    <meshBasicMaterial attach="material" color="white" />
+                </mesh>
+            </group>
+        );
+    });
+
+    return generatedTiles
+}
+
+
+function Scene() {
+    return (
+        <>
+            <Controls />
+            <TileGenerator
+                halfTilesX={5}
+                halfTilesZ={5}
+                tileSize={10} />
+        </>
+    );
+}
+
 
 export default function Release0009_Javonntte({ }) {
+
+    // TODO: the id for Canvas should be "canvas" and its css should live alongside a generic release canvas
     return (
-        <Canvas
-            gl={glConfig}
-        >
-            <Thing vertices={[[-1, 0, 0], [0, 1, 0], [1, 0, 0], [0, -1, 0], [-1, 0, 0]]} />
-            <Controls />
+        <Canvas id="root">
+            <Scene />
         </Canvas>
     );
 }
