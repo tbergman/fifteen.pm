a6a8d5c4bc03824e6597431f1dc2a6d03a4b1277
jaak <dev@globally.ltd>
2019-08-23 15:20:42 -0400

    Punt on intelligent tile-filling

--- src/Utils/TileGenerator.js
+++ src/Utils/TileGenerator.js
@@ -2,8 +2,27 @@ import React, { useRef, useState } from 'react';
 import { useRender, useThree } from 'react-three-fiber';
 import * as THREE from 'three';
 
+
+// const visibleHeightAtZDepth = (depth, camera) => {
+//     // compensate for cameras not positioned at z=0
+//     const cameraOffset = camera.position.z;
+//     if (depth < cameraOffset) depth -= cameraOffset;
+//     else depth += cameraOffset;
+
+//     // vertical fov in radians
+//     const vFOV = camera.fov * Math.PI / 180;
+
+//     // Math.abs to ensure the result is always positive
+//     return 2 * Math.tan(vFOV / 2) * Math.abs(depth);
+// };
+
+// const visibleWidthAtZDepth = (depth, camera) => {
+//     const height = visibleHeightAtZDepth(depth, camera);
+//     return height * camera.aspect;
+// };
+
 export default function TileGenerator({ tileSize, grid, tileComponent, tileResources }) {
-    const { camera, scene, size  } = useThree();
+    const { camera, scene, size } = useThree();
     const tiles = useRef({});
     const [lastUpdateTime, setLastUpdateTime] = useState(0);
     const boundary = useRef({ x: 0, z: 0 });
@@ -24,10 +43,8 @@ export default function TileGenerator({ tileSize, grid, tileComponent, tileResou
         const halfTiles = Math.floor(grid / 2);
         const halfTilesX = halfTiles;
         const halfTilesY = halfTiles;
-        const halfTilesX_ = Math.floor((size.width + tileSize)/tileSize);
-        const halfTilesY_ = Math.floor((size.height + tileSize)/tileSize);
-        console.log(halfTilesX_, size.width, tileSize);
-
+        // TODO want to get something like this working: https://discourse.threejs.org/t/functions-to-calculate-the-visible-width-height-at-a-given-z-depth-from-a-perspective-camera/269/19
+        // so that the tiles intelligently fill the visible screen with a buffer
         for (let x = -halfTilesX; x < halfTilesX; x++) {
             for (let z = -halfTilesY; z < halfTilesY; z++) {
                 const pos = new THREE.Vector3((x * tileSize + cameraX), 0, (z * tileSize + cameraZ));
