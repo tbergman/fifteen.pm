c7f1c32b0b4bd3183a6749d79fa121c440bf545a
jaak <dev@globally.ltd>
2019-08-26 14:40:46 -0400

    Make the line thinner

--- src/Shaders/tronFragment.glsl
+++ src/Shaders/tronFragment.glsl
@@ -8,7 +8,7 @@ void main()
     // Time varying pixel color
     vec3 col = vec3(0.);
     // TODO - make this 'wrap' around the screen
-    if (vUv.y > .75){
+    if (vUv.y > .95){
         col.x =sin(vUv.x + uPosOffset.x - uTime - uCurCenter.x);
     }
     gl_FragColor = vec4( col, 1.0 );
