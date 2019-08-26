a76fc468aae5e1d690c87a9935a71a797c1ae8a2
jaak <dev@globally.ltd>
2019-08-26 14:40:31 -0400

    Use sin wav for simple wraparound

--- src/Shaders/tronFragment.glsl
+++ src/Shaders/tronFragment.glsl
@@ -7,8 +7,9 @@ void main()
 {    
     // Time varying pixel color
     vec3 col = vec3(0.);
+    // TODO - make this 'wrap' around the screen
     if (vUv.y > .75){
-        col.x =vUv.x + uPosOffset.x - uTime - uCurCenter.x;
+        col.x =sin(vUv.x + uPosOffset.x - uTime - uCurCenter.x);
     }
     gl_FragColor = vec4( col, 1.0 );
 }
