0e472212d3c80f8045bcb1db7310b2e1998d4562
jaak <dev@globally.ltd>
2019-09-05 11:43:03 -0400

    Check distance more often for smoother loading

--- src/Releases/Release0009_Javonntte/face.js
+++ src/Releases/Release0009_Javonntte/face.js
@@ -75,12 +75,71 @@ function generateTiles(sphereGeometry, boundary) {
     return tiles;
 }
 
+function bucketTiles(radius, tiles) {
+    // 8 sections.
+    //  (radius is divisible by 8...)
+    const bucketedTiles = {};
+    const sections = [];
+    const sectionSize = 3; // (radius / 8)
+    for (let y = 0; y < 8; y++) {
+        const startYVal = y * sectionSize;
+        const endYVal = startYVal + sectionSize;
+        for (let z = 0; z < 8; z++) {
+            const startZVal = z * sectionSize;
+            const endZVal = startZVal + sectionSize;
+            // positives and negatives
+            sections.push(
+                {
+                    minY: startYVal,
+                    maxY: endYVal,
+                    minZ: startZVal,
+                    maxZ: endZVal,
+                },
+                {
+                    minY: -endYVal,
+                    maxY: -startYVal,
+                    minZ: -endZVal,
+                    maxZ: -startZVal,
+                },
+                {
+                    minY: startYVal,
+                    maxY: endYVal,
+                    minZ: -endZVal,
+                    maxZ: -startZVal,
+                },
+                {
+                    minY: -endYVal,
+                    maxY: -startYVal,
+                    minZ: startZVal,
+                    maxZ: endZVal,
+                }
+            )
+        }
+    }
+    for (let i = 0; i < sections.length; i++) {
+        bucketedTiles[i] = [];
+    }
+    Object.values(tiles).forEach(tile => {
+        sections.forEach((section, index) => {
+            if (tile.centroid.x < section.maxY && tile.centroid.x >= section.minY &&
+                tile.centroid.z < section.maxZ && tile.centroid.z >= section.minZ) {
+                bucketedTiles[index].push(tile);
+            }
+        })
+    })
+    return [bucketedTiles, sections];
+}
+
 // https://discourse.threejs.org/t/raycast-objects-that-arent-in-scene/6704/4 
 // view-source:https://rawgit.com/pailhead/three.js/instancing-part2-200k-instanced/examples/webgl_interactive_cubes.html
 // https://medium.com/@pailhead011/instancing-with-three-js-part-2-3be34ae83c57
 export function TileGenerator({ radius, sides, tiers, tileComponent, geometries, startPos, maxHeight }) {
     const { camera, scene, raycaster } = useThree();
     const [lastUpdateTime, setLastUpdateTime] = useState(0);
+    const section = useRef(0);
+    // const [section.current, setCurrentSection] = useState(0);
+    const bucketedTiles = useRef({});
+    const sections = useRef([]);
     const boundary = useRef(new THREE.Vector3().copy(startPos));
     const prevBoundary = useRef(boundary.current.clone());
     const tilesGroup = useRef(new THREE.Group());
@@ -97,26 +156,55 @@ export function TileGenerator({ radius, sides, tiers, tileComponent, geometries,
                 return tile;
             }
         });
+        [bucketedTiles.current, sections.current] = bucketTiles(radius, allTiles.current);
+        // console.log("bucketed tiles", bucketedTiles.current)
+        section.current = 0;
     }, [])
 
+
     useRender((state, time) => {
-        const rotXDelta = .001;
+        const rotXDelta = .0001;
         tilesGroup.current.rotation.x += rotXDelta; // TODO these are just mimicking rotation in world, no bueno
-        tilesGroup.current.rotation.x = tilesGroup.current.rotation.x % (2 * Math.PI);
-        tilesGroup.current.rotation.z = -raycaster.ray.direction.x * .6; // TODO these are just mimicking rotation in world, no bueno
-        tilesGroup.current.rotation.z = tilesGroup.current.rotation.z % (2 * Math.PI);
+        // tilesGroup.current.rotation.x = tilesGroup.current.rotation.x % (2 * Math.PI);
+        // tilesGroup.current.rotation.z = raycaster.ray.direction.x; // TODO these are just mimicking rotation in world, no bueno
+        // tilesGroup.current.rotation.z = tilesGroup.current.rotation.z % (2 * Math.PI);
         const xRadiansRot = tilesGroup.current.rotation.x;
-        const zRadiansRot = tilesGroup.current.rotation.z;
-        // const xDegrees = xRadiansRot * 180 / Math.PI;
-        // const zDegrees = zRadiansRot * 180 / Math.PI;
+        // const zRadiansRot = tilesGroup.current.rotation.z;
         const curZOffset = Math.cos(xRadiansRot) * radius;
         const curYOffset = Math.sin(xRadiansRot) * radius;
-        const curXOffset = Math.sin(zRadiansRot) * radius;
-        boundary.current.x = -curXOffset;
+        // const curXOffset = Math.sin(zRadiansRot) * radius;
+        // boundary.current.x = -curXOffset;
         boundary.current.z = -curZOffset;
         boundary.current.y = -curYOffset;
-            // let maxY, maxZ, minY, minZ;
-        if (prevBoundary.current.distanceTo(boundary.current) > 5) {
+        // let maxY, maxZ, minY, minZ;
+        // TODO maybe this will just work with one dimension?
+        // TODO bug... it's late
+        // if (!section.current) section.current = 0;   
+
+        // const previousSection = section.current;
+        // for (let i = 0; i < sections.current.length; i++) {
+        //     if (boundary.current.z < sections.current[i].maxZ &&
+        //         boundary.current.z >= sections.current[i].minZ &&
+        //         boundary.current.y < sections.current[i].maxY &&
+        //         boundary.current.y >= sections.current[i].minY) {
+        //         if (section.current !== i) {
+        //             // bucketedTiles.current[previousSection].forEach(tile => {
+        //             //     tile.visible = false;
+        //             // }) // TODO fix turning off
+        //             section.current = i;
+        //             visibleTiles.current = bucketedTiles.current[section.current].map(tile => {
+        //                 console.log("TLIE", tile);
+        //                 if (tile.visible === true) tile.hasRendered = true;
+        //                 else tile.visible = true;
+        //                 return tile;
+        //             });
+        //             console.log('visible tiles', visibleTiles.current);
+        //         }
+        //         prevBoundary.current.copy(boundary.current);
+        //     }
+        // }
+        // if (section.current !== previousSection) setLastUpdateTime(time);
+        if (prevBoundary.current.distanceTo(boundary.current) > .5) {
             setLastUpdateTime(time);
             visibleTiles.current = Object.values(allTiles.current).filter(tile => {
                 // TODO this could all be calculated once rather than interated thru once it's working...
@@ -134,12 +222,10 @@ export function TileGenerator({ radius, sides, tiers, tileComponent, geometries,
                     allTiles.current[tile.id].visible = false;
                 }
             })
-            // console.log('maxy', maxY, 'miny', minY, 'maxz', maxZ, 'minz', minZ);
             prevBoundary.current.copy(boundary.current);
         }
     });
 
-    // console.log("RenderTiles", visibleTiles.current.length);
     return <group ref={tilesGroup}>
         {visibleTiles.current && visibleTiles.current.map(props => {
             return <group key={props.id}>
