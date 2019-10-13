50ebef6eaae6ff076a5be3e1fa6c16bc0df2a72b
jak2030 <krinsley@gmail.com>
2019-10-13 19:39:06 -0400

    Hacking on rainbow road tile world 2

--- src/Releases/Release0009_Javonntte/Road.js
+++ src/Releases/Release0009_Javonntte/Road.js
@@ -26,64 +26,66 @@ function buildPath({ startPos, stepSize, numPathSteps }) {
 export default function Road({ tubeProps: { scale, extrusionSegments, radius, radiusSegments, closed, offset }, ...props }) {
 
     const { camera } = useThree();
-    const stepSize = 10;
-    const numPathSteps = 20;
+    const stepSize = 100;
+    const numPathSteps = 10;
     const numSteps = 20000;
     // const threshold = numSteps * stepSize * .9; // TODO naming and pass val in as prop
 
     const [lastUpdateTime, setLastUpdateTime] = useState(0);
     const [generatingRoad, setGeneratingRoad] = useState(false);
-    const road = useRef();
+    const curSection = useRef();
+    const nextSection = useRef();
     const steps = useRef();
     const step = useRef();
     const [cloudMaterialRef, cloudMaterial] = useResource();
     const normal = new THREE.Vector3(0, 0, 0);
     const binormal = new THREE.Vector3(0, 1, 0);
-    const boundary = useRef({ x: 0, z: 0 }); // trigger tile generation on load
-
-    function shouldAddRoad() {
-        if (!steps.current) return true;
-        if (generatingRoad) return false;
-        const prevPos = boundary.current;
-        const curPos = camera.position;
-        const xMove = Math.abs(curPos.x - prevPos.x);
-        const zMove = Math.abs(curPos.z - prevPos.z);
-        return xMove >= threshold || zMove >= threshold;
-    }
 
     useEffect(() => {
         // boundary.current = { x: camera.position.x, z: camera.position.z };
-        const startPos = steps.current ? steps.current[steps.current.length - 1] : camera.position;
-        steps.current = buildPath({ startPos, stepSize, numPathSteps })
+        const nextSteps = [];
+        if (steps.current){
+            nextSteps.push(steps.current[steps.current.length - 2]);
+            nextSteps.push(steps.current[steps.current.length - 1]);
+        } else {
+            nextSteps.push(camera.position);
+        }
+        const startPos = nextSteps[nextSteps.length - 1];
+        nextSteps.push(...buildPath({ startPos, stepSize, numPathSteps }));
+        steps.current = nextSteps;
         console.log("ADD ROAD w", steps.current)
         var closedSpline = new THREE.CatmullRomCurve3(steps.current);
         const tubeGeometry = new THREE.TubeBufferGeometry(closedSpline, extrusionSegments, radius, radiusSegments, closed);
-        road.current = tubeGeometry;
+        if (!curSection.current) curSection.current = tubeGeometry;
+        else nextSection.current = tubeGeometry;
         setGeneratingRoad(false);
     }, [generatingRoad])
 
     // Drive camera along road
     useRender((state, time) => {
-        // if (!step.current) step.current = 0;
-        // var timeN = Date.now(); 
         var t = (time % numSteps) / numSteps;
-        if (t == 1 && !generatingRoad) {
+        if (!curSection.current || (t >= .2 && !generatingRoad && !nextSection.current)) {
             console.log("GENERATING ROAD")
-        //     setGeneratingRoad(true);
-        //     t = 0;
+            setGeneratingRoad(true);
         }
-        var pos = road.current.parameters.path.getPointAt(t);
+        if (t >= .9 && nextSection.current) {
+            console.log("SETTING NEXT ROAD SECTION")
+            curSection.current = nextSection.current;
+            nextSection.current = undefined;
+            t = 2 / steps.current.length;
+        }
+        var pos = curSection.current.parameters.path.getPointAt(t);
         // console.log('pos on road', pos, 'cam pos', camera.position);
         pos.multiplyScalar(scale);
         // interpolation
-        var segments = road.current.tangents.length;
+        var segments = curSection.current.tangents.length;
         var pickt = t * segments;
         var pick = Math.floor(pickt);
         var pickNext = (pick + 1) % segments;
-        console.log("t", t, 'pickNext', pickNext, 'pick', pick, 'pickt', pickt, 'num segs', segments)
-        binormal.subVectors(road.current.binormals[pickNext], road.current.binormals[pick]);
-        binormal.multiplyScalar(pickt - pick).add(road.current.binormals[pick]);
-        var dir = road.current.parameters.path.getTangentAt(t);
+        // console.log("t", t, 'pickNext', pickNext, 'pick', pick, 'pickt', pickt, 'num segs', segments)
+        binormal.subVectors(curSection.current.binormals[pickNext], curSection.current.binormals[pick]);
+        binormal.multiplyScalar(pickt - pick).add(curSection.current.binormals[pick]);
+        var dir = curSection.current.parameters.path.getTangentAt(t);
         normal.copy(binormal).cross(dir);
         // We move on a offset on its binormal
         pos.add(normal.clone().multiplyScalar(offset));
@@ -91,7 +93,7 @@ export default function Road({ tubeProps: { scale, extrusionSegments, radius, ra
         camera.position.copy(pos);
         // Using arclength for stablization in look ahead.
         // var lookAt = road.current.parameters.path.getPointAt((step.current + 1) / numSteps).multiplyScalar(scale);
-        var lookAt = road.current.parameters.path.getPointAt((t + 30 / road.current.parameters.path.getLength()) % 1).multiplyScalar(scale);
+        var lookAt = curSection.current.parameters.path.getPointAt((t + 30 / curSection.current.parameters.path.getLength()) % 1).multiplyScalar(scale);
         // Camera Orientation 2 - up orientation via normal
         lookAt.copy(pos).add(dir);
         camera.matrix.lookAt(camera.position, lookAt, normal);
@@ -103,9 +105,16 @@ export default function Road({ tubeProps: { scale, extrusionSegments, radius, ra
     // if (road.current) console.log('road array idx 0:', road.current.attributes.position.array[0])
     return <>
         <CloudMaterial materialRef={cloudMaterialRef} emissive={0xd4af37} />
-        {cloudMaterialRef && road.current &&
+        {cloudMaterialRef && curSection.current &&
             <mesh
-                geometry={road.current}
+                geometry={curSection.current}
+                material={cloudMaterial}
+                scale={[scale, scale, scale]}
+            />
+        }
+        {nextSection.current &&
+            <mesh
+                geometry={nextSection.current}
                 material={cloudMaterial}
                 scale={[scale, scale, scale]}
             />
