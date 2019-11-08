5190d87aa822fbd6d728c260392338e7d6a712e2
jak2030 <krinsley@gmail.com>
2019-11-07 22:14:40 -0500

    Mobile navigation added

--- src/Releases/Release0009_Javonntte/car/DashCam.js
+++ src/Releases/Release0009_Javonntte/car/DashCam.js
@@ -17,29 +17,18 @@ export default function DashCam({ target }) {
         window.addEventListener("touchmove", touchLook, false);
     })
 
-    const [euler, PI_2] = useMemo(() => [new THREE.Euler(0, 0, 0, 'YXZ'), Math.PI / 2])
+    const [euler, PI_7, PI_11, PI_24] = useMemo(() => [new THREE.Euler(0, 0, 0, 'YXZ'), Math.PI / 7, Math.PI / 11, Math.PI / 24])
 
     const touchLook = (event) => {
         if (!ref.current) return;
-
-        //   this.setMouseCoords(event.touches[0].clientX, event.touches[0].clientY);
-        // var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
-        // console.log("EVENT", event)
         var movementX = (event.touches[0].clientX - window.innerWidth / 2) || 0;
         var movementY = (event.touches[0].clientY - window.innerHeight / 2) || 0;
-
-        console.log('Event Touches: ', event.touches)
-        console.log("MOVX", movementX, "MOVY", movementY)
-
         euler.setFromQuaternion(ref.current.quaternion);
-
-        euler.x -= movementY * 0.00002;
-        euler.y -= movementX * 0.00002;
-
-        euler.x = Math.max(- PI_2, Math.min(PI_2, euler.x));
+        euler.x -= movementY * 0.00005;
+        euler.y -= movementX * 0.00005;
+        euler.y = Math.max(- PI_11, Math.min(PI_11, euler.y));
+        euler.x = Math.max(- PI_24, Math.min(PI_7, euler.x));
         ref.current.quaternion.setFromEuler(euler);
-
-        //scope.dispatchEvent( { type: 'change' });
     }
 
     // Make the camera known to the system
@@ -52,35 +41,21 @@ export default function DashCam({ target }) {
     // Update it every frame
     useFrame(() => ref.current.updateMatrixWorld())
     const controls = useRef();
-    // const { gl } = useThree();
-    const delta = .001;
-    // useFrame(() => { controls.current && controls.current.update(delta) });
-
-    console.log('target', target)
-
     return <>
         <perspectiveCamera
             ref={ref}
             aspect={size.width / size.height}
             radius={(size.width + size.height) / 4}
             fov={55}
+            near={.01}
             position={[0, .068, .15]}
             onUpdate={self => self.updateProjectionMatrix()}
         />
         {ref.current &&
-            // (isMobile ?
-            //     <orbitControls
-            //     ref={controls}
-            //     target={target}
-            //     args={[ref.current, gl.domElement]}
-            //     enablePan={false}
-            //     />
-            //     :
             <pointerLockControls
                 ref={controls}
                 args={[ref.current, gl.domElement]}
                 isLocked={true} />
-            // )
         }
     </>
 }
