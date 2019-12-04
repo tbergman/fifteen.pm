b49f19facda6b5f42b443002ec8c5c5f4f0f4a2a
jak2030 <krinsley@gmail.com>
2019-12-03 22:36:36 -0500

    Fixed froggy

--- src/Releases/Release0010_AlienD/Frog.js
+++ src/Releases/Release0010_AlienD/Frog.js
@@ -16,39 +16,43 @@ export default function Frog(props) {
 
   const { foamGripPurple } = useContext(MaterialsContext);
 
-  const mesh = useMemo(()=> {
+  const mesh = useMemo(() => {
     if (!gltf) {
       return;
     }
-    const geom = gltf.__$[31];
-    console.log('geom' , geom);
-    geom.toNonIndexed();
-    geom.computeBoundingBox();
-    return new THREE.InstancedMesh( geom, foamGripPurple, 100 );
+    let geom;
+    gltf.scene.traverse(child => {
+      if (child.isMesh) {
+        geom = child.geometry;
+        geom.toNonIndexed();
+        geom.computeBoundingBox();
+      }
+    })
+    return new THREE.InstancedMesh(geom, foamGripPurple, 100);
   }, [gltf])
 
   var amount = 10;
   const dummy = new THREE.Object3D();
   useFrame(() => {
-      if ( mesh ) {
-        var time = Date.now() * 0.001;
-        mesh.rotation.x = Math.sin( time / 4 );
-        mesh.rotation.y = Math.sin( time / 2 );
-        var i = 0;
-        var offset = ( amount - 1 ) / 2;
-        for ( var x = 0; x < amount; x ++ ) {
-          for ( var y = 0; y < amount; y ++ ) {
-            for ( var z = 0; z < amount; z ++ ) {
-              dummy.position.set( offset - x, offset - y, offset - z );
-              dummy.rotation.y = ( Math.sin( x / 4 + time ) + Math.sin( y / 4 + time ) + Math.sin( z / 4 + time ) );
-              dummy.rotation.z = dummy.rotation.y * 2;
-              dummy.updateMatrix();
-              mesh.setMatrixAt( i ++, dummy.matrix );
-            }
+    if (mesh) {
+      var time = Date.now() * 0.001;
+      mesh.rotation.x = Math.sin(time / 4);
+      mesh.rotation.y = Math.sin(time / 2);
+      var i = 0;
+      var offset = (amount - 1) / 2;
+      for (var x = 0; x < amount; x++) {
+        for (var y = 0; y < amount; y++) {
+          for (var z = 0; z < amount; z++) {
+            dummy.position.set(offset - x, offset - y, offset - z);
+            dummy.rotation.y = (Math.sin(x / 4 + time) + Math.sin(y / 4 + time) + Math.sin(z / 4 + time));
+            dummy.rotation.z = dummy.rotation.y * 2;
+            dummy.updateMatrix();
+            mesh.setMatrixAt(i++, dummy.matrix);
           }
         }
-        mesh.instanceMatrix.needsUpdate = true;
       }
+      mesh.instanceMatrix.needsUpdate = true;
+    }
   })
   return (
     <group ref={group} {...props}>
