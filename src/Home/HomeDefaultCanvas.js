d1ea68a03dfa79ba79751abb57a9540a5f344d1d
Jeremy Krinsley <krinsley@gmail.com>
2020-04-15 23:21:04 -0400

    rm water from homepage

--- src/Home/HomeDefaultCanvas.js
+++ src/Home/HomeDefaultCanvas.js
@@ -19,7 +19,7 @@ export class HomeDefaultCanvas extends PureComponent {
     componentDidMount() {
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0xFF0FFF);
-        this.camera = new THREE.PerspectiveCamera(24, window.innerWidth / window.innerHeight, 1, 100);
+        this.camera = new THREE.PerspectiveCamera(24, window.innerWidth / window.innerHeight, 8, 48);
         this.camera.position.set(2.095357312111118, 0.8505415314262585, 11.67921580831209);
         this.camera.rotation.set(-0.042438732895147505, 0.3365395324927288, 0.01402173472318894);
         this.camera.lookAt(new THREE.Vector3());
@@ -28,6 +28,8 @@ export class HomeDefaultCanvas extends PureComponent {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         window.addEventListener('resize', this.onWindowResize, false);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+        this.controls.autoRotate = true;
+        this.controls.autoRotateSpeed = 5;
         this.init();
         this.animate();
     }
@@ -64,7 +66,7 @@ export class HomeDefaultCanvas extends PureComponent {
         const manager = new THREE.LoadingManager();
         const gltfLoader = new GLTFLoader(manager);
         const textureLoader = new THREE.TextureLoader();
-        const rockMaterial = initRockMaterial(textureLoader, 0xFFAFFF, {displacementScale: 1.5});
+        const rockMaterial = initRockMaterial(textureLoader, 0xFFAFFF, {displacementScale: 1.1 });
         rockMaterial.displacementBias = -30;
         const name = "forest";
         // add rocks
@@ -75,7 +77,7 @@ export class HomeDefaultCanvas extends PureComponent {
             rotateX: 0,
             rotateY: 0,
             rotateZ: 0,
-            relativeScale: 1,
+            relativeScale: 2,
             loader: gltfLoader,
             onSuccess: (gltf) => {
                 const { scene, textureLoader } = this;
@@ -92,25 +94,25 @@ export class HomeDefaultCanvas extends PureComponent {
         }
         loadGLTF({ ...gltfParams });
         // add water
-        const params = {
-            color: '#ffffff',
-            scale: 4,
-            flowX: 1,
-            flowY: 1
-        };
-        const waterGeometry = new THREE.PlaneBufferGeometry(20, 20);
-        const water = new Water(waterGeometry, {
-            color: params.color,
-            scale: params.scale,
-            flowDirection: new THREE.Vector2(params.flowX, params.flowY).normalize(),
-            textureWidth: 512,
-            textureHeight: 512,
-            normalMap0: textureLoader.load('assets/shared/images/water/textures/water/Water_1_M_Normal.jpg'),
-            normalMap1: textureLoader.load('assets/shared/images/water/textures/water/Water_2_M_Normal.jpg'),
-        });
-        water.position.y = .1;
-        water.rotation.x = Math.PI * -0.5;
-        scene.add(water);
+        // const params = {
+        //     color: '#ffffff',
+        //     scale: 4,
+        //     flowX: 1,
+        //     flowY: 1
+        // };
+        // const waterGeometry = new THREE.PlaneBufferGeometry(20, 20);
+        // const water = new Water(waterGeometry, {
+        //     color: params.color,
+        //     scale: params.scale,
+        //     flowDirection: new THREE.Vector2(params.flowX, params.flowY).normalize(),
+        //     textureWidth: 512,
+        //     textureHeight: 512,
+        //     normalMap0: textureLoader.load('assets/shared/images/water/textures/water/Water_1_M_Normal.jpg'),
+        //     normalMap1: textureLoader.load('assets/shared/images/water/textures/water/Water_2_M_Normal.jpg'),
+        // });
+        // water.position.y = .1;
+        // water.rotation.x = Math.PI * -0.5;
+        // scene.add(water);
     }
 
     animate = () => {
@@ -122,6 +124,7 @@ export class HomeDefaultCanvas extends PureComponent {
         const { renderer, camera, scene } = this;
         // this.renderCursorStyle();
         // this.renderReleaseLinks();
+        this.controls.update()
         renderer.render(scene, camera);
     }
 
