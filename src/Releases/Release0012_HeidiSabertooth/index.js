58c8e7b9af2f189318c7c387b09ac1b868cd3f06
Jeremy Krinsley <krinsley@gmail.com>
2020-06-23 01:24:23 -0400

    heidi scaffolding

---
+++ src/Releases/Release0012_HeidiSabertooth/index.js
@@ -0,0 +1,14 @@
+import React, { useMemo } from 'react';
+import { CONTENT } from '../../Content';
+import { AudioPlayerProvider } from '../../Common/UI/Player/AudioPlayerContext';
+import Release from './Release';
+import "./index.css";
+
+export default function Release0012_HeidiSabertooth({ }) {
+    const tracks = useMemo(() => CONTENT[window.location.pathname].tracks)
+    return (
+        <AudioPlayerProvider tracks={tracks}>
+            <Release />
+        </AudioPlayerProvider >
+    );
+}
