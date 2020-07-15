0172eb1888eeac13fc154085ca00a6f87af81055
Jeremy Krinsley <krinsley@gmail.com>
2020-07-14 22:50:35 -0400

    refactor in progress

--- src/Releases/Release0009_Javonntte/car/Car.js
+++ src/Releases/Release0009_Javonntte/car/Car.js
@@ -12,7 +12,7 @@ import Dashboard from './Dashboard';
 import DashCam from './DashCam';
 import Headlights from './Headlights';
 import SteeringWheel from './SteeringWheel';
-
+import {getCurTrajectory} from '../../../Common/Animations/SplineAnimator.js'
 function Car({
     headlightsColors,
     road,
@@ -72,23 +72,6 @@ function Car({
             }
 
         }
-    }
-
-    function getCurTrajectory(t) {
-        const pos = road.parameters.path.getPointAt(t);
-        offset.current += delta.current;
-        // interpolation
-        const segments = road.tangents.length;
-        const pickt = t * segments;
-        const pick = Math.floor(pickt);
-        const pickNext = (pick + 1) % segments;
-        binormal.subVectors(road.binormals[pickNext], road.binormals[pick]);
-        binormal.multiplyScalar(pickt - pick).add(road.binormals[pick]);//.add(up);
-        const dir = road.parameters.path.getTangentAt(t);
-        normal.copy(binormal); // most examples have .cross(dir) here but this will rotate the normal to the 'side' of the orientation we want to achieve 
-        // We move on a offset on its binormal
-        pos.add(normal.clone());
-        return [pos, dir];
     }
 
     const spinLeft = () => {
