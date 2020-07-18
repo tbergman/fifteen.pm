9bb147b2739e2523bf6739fd02ca052f8dd73eb1
Jeremy Krinsley <krinsley@gmail.com>
2020-07-18 00:27:45 -0400

    moving texture

--- src/Releases/Release0012_HeidiSabertooth/Catwalk.js
+++ src/Releases/Release0012_HeidiSabertooth/Catwalk.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useMemo, useContext, useRef, useState } from 'react';
-import { useThree } from 'react-three-fiber';
+import { useThree, useFrame } from 'react-three-fiber';
 import * as THREE from 'three';
 import { MaterialsContext } from './MaterialsContext';
 
 export default function Catwalk({ extrusionSegments, radius, radiusSegments, ...props }) {
     const ref = useRef();
-    // const { polishedSpeckledMarbleTop } = useContext(MaterialsContext);
+    const { wireframe, platformPolishedSpeckledMarbleTop, linedCement } = useContext(MaterialsContext);
     const [catwalk, setCatwalk] = useState()
     const [platform, setPlatform] = useState()
     useEffect(() => {
         const steps = [
-            new THREE.Vector3(-5, 0, 0),
-            new THREE.Vector3(10, 7.5, 0),
-            new THREE.Vector3(5, 0, 0),
+            new THREE.Vector3(-10, -5, 0),
+            new THREE.Vector3(-11, 0, 0),
+            new THREE.Vector3(-10, 4, 0),
+            new THREE.Vector3(0, 5, 0),
+            new THREE.Vector3(10, 4, 0),
+            new THREE.Vector3(10, -4, 0),
+            new THREE.Vector3(0, -5, 0),
         ]
         var closedSpline = new THREE.CatmullRomCurve3(steps);
         closedSpline.closed = true;
@@ -20,11 +24,17 @@ export default function Catwalk({ extrusionSegments, radius, radiusSegments, ...
         radius = radius ? radius : 1
         radiusSegments = radiusSegments ? radiusSegments : 100
         extrusionSegments = extrusionSegments ? extrusionSegments : radiusSegments
-        const tubeGeometry = new THREE.TubeBufferGeometry(closedSpline, extrusionSegments, radius, radiusSegments, closedSpline.closed);
-        setCatwalk(tubeGeometry)
+        // const geometry = new THREE.TubeBufferGeometry(closedSpline, extrusionSegments, radius, radiusSegments, closedSpline.closed);
+        const geometry = new THREE.TubeBufferGeometry(closedSpline, 100, 1, 4, closedSpline.closed);
+        setCatwalk(geometry)
     }, [])
+    useFrame(() => {
+        // TODO (jeremy) this speed should change per track
+        platformPolishedSpeckledMarbleTop.map.offset.x -= .01;
+        // linedCement.map.offset.x -= .001;
+    })
     return <group ref={ref}>
-        <mesh geometry={catwalk} />
+        <mesh geometry={catwalk} material={platformPolishedSpeckledMarbleTop} />
         <group >
             {React.Children.toArray(props.children).map(child => {
                 return React.cloneElement(child, { catwalk: catwalk })
--- src/Releases/Release0012_HeidiSabertooth/MaterialsContext.js
+++ src/Releases/Release0012_HeidiSabertooth/MaterialsContext.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useResource } from 'react-three-fiber';
 import PolishedSpeckledMarbleTop from '../../Common/Materials/PolishedSpeckledMarbleTop';
-
+import LinedCement from '../../Common/Materials/LinedCement';
 
 const MaterialsContext = React.createContext([{}, () => { }]);
 
 const MaterialsProvider = ({ ...props }) => {
     const [loaded, setLoaded] = useState(false);
-
+const[wireframeRef, wireframe] = useResource()
+    const [linedCementRef, linedCement] = useResource();
     const [polishedSpeckledMarbleTopRef, polishedSpeckledMarbleTop] = useResource();
-
+    const [platformPolishedSpeckledMarbleTopRef, platformPolishedSpeckledMarbleTop] = useResource();
     const materials = {
+        linedCement,
         polishedSpeckledMarbleTop,
+        wireframe,
+        platformPolishedSpeckledMarbleTop,
     }
 
     useEffect(() => {
@@ -23,7 +27,19 @@ const MaterialsProvider = ({ ...props }) => {
     return <MaterialsContext.Provider value={{ loaded, ...materials }}>
         <PolishedSpeckledMarbleTop
             materialRef={polishedSpeckledMarbleTopRef}
+            textureRepeat={{ x: 16, y: 16 }}
             skinning={true}
+        />
+        <PolishedSpeckledMarbleTop
+            materialRef={platformPolishedSpeckledMarbleTopRef}
+            textureRepeat={{ x: 8, y: 8 }}
+        /> 
+        <LinedCement 
+            materialRef={linedCementRef}
+        />
+        <meshStandardMaterial
+            ref={wireframeRef}
+            wireframe={true}
         />
         {props.children}
     </MaterialsContext.Provider>
